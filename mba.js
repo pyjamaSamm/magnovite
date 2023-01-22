0a95aad46925d701fba4bdd2307beb76d50d0399
Ayush yadav <20ayush07yadav03@gmail.com>
2023-01-22 18:30:49 +0530

    changes

---
+++ mba.js
@@ -0,0 +1,32 @@
+// const scroll1 = document.querySelector('.scroll1')
+// const scroll2 = document.querySelector('.scroll2')
+// const img1 = document.querySelector('.img1');
+// const img2 = document.querySelector('.img2');
+// const half2 = document.querySelector('.half2');
+
+// scroll2.onmouseover = function() {
+    
+//     img2.classList.remove("displayoff");
+//     img2.classList.remove("displayon");
+
+// }
+
+// half2.addEventListener("scroll", (event) => {
+    
+//     console.log(getOffset(img2).top)
+// })
+
+
+// console.log(getOffset(img2).top)
+// function getOffset( el ) {
+//     var _x = 0;
+//     var _y = 0;
+//     while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+//         _x += el.offsetLeft - el.scrollLeft;
+//         _y += el.offsetTop - el.scrollTop;
+//         el = el.offsetParent;
+//     }
+//     return { top: _y, left: _x };
+// }
+
+
